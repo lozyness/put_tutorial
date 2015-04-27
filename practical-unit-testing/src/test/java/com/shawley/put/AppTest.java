aa59e96cc9c86f572b1535ec8a1bf14850b902ad
Laura Turley <lozyness@gmail.com>
2015-04-27 21:36:15 +0100

    Basic Hello World with maven build

---
+++ practical-unit-testing/src/test/java/com/shawley/put/AppTest.java
@@ -0,0 +1,20 @@
+package com.shawley.put;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
